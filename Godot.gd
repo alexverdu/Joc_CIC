482687c84eae140267c3f609fb6b1d67b63a6ee3
alexverdu <101245851+alexverdu@users.noreply.github.com>
2022-03-14 12:29:58 +0100

    Colisions + color

--- Godot.gd
+++ Godot.gd
@@ -1,14 +1,26 @@
-extends Sprite
+extends Area2D
 
-export var velocitat : int = 300
-var direccio = Vector2(0, 0) 
+export var velocitat : int = 700
+var direccio = Vector2.ZERO
 
 func _ready():
 	rotation_degrees = 90
 	position = Vector2(120, 500)
 	
 func _process(delta):
-	position += direccio * velocitat * delta
-		
+	position += direccio * velocitat * delta	
 	if Input.is_action_pressed("mou_dreta"):
-		direccio = Vector2(1, 0)
+		direccio += Vector2.RIGHT
+	if Input.is_action_pressed("mou_esquerra"):
+		direccio += Vector2.LEFT
+	if Input.is_action_pressed("mou_adalt"):
+		direccio += Vector2.UP
+	if Input.is_action_pressed("mou_abaix"):
+		direccio += Vector2.DOWN
+		
+	position += direccio.normalized()                                                                  * velocitat * delta
+	direccio = Vector2.ZERO	
+
+
+func _on_Personatges_area_entered(area):
+	modulate = Color(1,0,0)
